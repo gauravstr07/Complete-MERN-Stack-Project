6559843d8c285bb9816d8b29ae64250008adcabc
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-11 01:13:16 +0530

    User Login In Backed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 require("./db/config");
 const User = require("./db/User");
 
@@ -12,7 +11,22 @@ const port = 5000;
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No user found!" });
+    }
+  } else {
+    res.send({ result: "Fill Feild Properly" });
+  }
 });
 
 app.listen(port, () => {
